12db90f889e71c93e9bd79158f628aefa0f67aa6
Alan G. Dawidowicz <alandawi@gmail.com>
2023-08-19 12:03:34 -0300

    initial commit

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+	let count = 0;
+
+    $: doble = count * 2;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+</button>
+
+<h2>{doble}</h2>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+    import Counter from "$lib/components/Counter.svelte";
+    import Hero from "$lib/components/Hero.svelte";
+    import Blocks from "$lib/components/Blocks.svelte";
+    import "$lib/css/style.css";
+
+    let imgSource = "https://picsum.photos/200/300";
+    let isActive = false;
+</script>
+
+<Header />
+
+{#if isActive}
+     <Counter />
+{/if}
+
+<Hero image={imgSource} />
+
+<Blocks />
+
+<style>
+    /* h2 {
+        background: red !important;
+    } */
+</style>
